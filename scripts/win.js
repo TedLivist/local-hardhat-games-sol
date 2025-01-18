3ec1477d7e3e4b34db1e1e33ebe4fe381b2e9084
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-01-18 14:02:27 +0100

    Win Game 5

--- scripts/win.js
+++ scripts/win.js
@@ -1,6 +1,6 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
-const contractName = "Game4";
+const gameAddr = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
@@ -8,7 +8,33 @@ async function main() {
 
     // do whatever you need to do to win the game here:
 
-    const tx = await game.win(56);
+    const signer = ethers.provider.getSigner(0);
+    const funder = ethers.provider.getSigner(1);
+
+    console.log(await signer.getAddress());
+    // const address = await signer.getAddress();
+    // console.log(await ethers.provider.getBalance(address));
+    console.log(await signer.getBalance());
+    console.log(ethers.utils.formatEther(await signer.getBalance()));
+
+    console.log(ethers.utils.formatEther(await funder.getBalance()));
+
+    const sendEth = await funder.sendTransaction({
+      to: signer.getAddress(),
+      value: ethers.utils.parseEther("1000.0")
+    });
+    await sendEth.wait();
+    console.log("Funded signer with 1000 ETH!")
+
+    console.log(ethers.utils.formatEther(await funder.getBalance()));
+    console.log(ethers.utils.formatEther(await signer.getBalance()));
+
+    const allow = await game.giveMeAllowance(10100);
+    await allow.wait();
+    const mint = await game.mint(10005);
+    await mint.wait();
+
+    const tx = await game.win();
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
