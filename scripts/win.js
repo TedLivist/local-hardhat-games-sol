ef3916c25bbdcf40d63b34de3ceb4991f08f5e09
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-01-18 13:21:16 +0100

    Win Game 4

--- scripts/win.js
+++ scripts/win.js
@@ -1,6 +1,6 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
-const contractName = "Game3";
+const gameAddr = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
+const contractName = "Game4";
 
 async function main() {
     // attach to the game
@@ -8,7 +8,7 @@ async function main() {
 
     // do whatever you need to do to win the game here:
 
-    const tx = await game.win(45);
+    const tx = await game.win(56);
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
